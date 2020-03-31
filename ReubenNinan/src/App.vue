7842715596c597dc5a34ab49ee515792979ae834
Reuben Mathew <intelphysic@gmail.com>
2020-03-31 17:10:28 -0400

    added 4 work showcases, todo: content, links and description

--- ReubenNinan/src/App.vue
+++ ReubenNinan/src/App.vue
@@ -1,12 +1,11 @@
 <template>
   <div id="app">
     <LandingPage/>
-    <div class="bg-gray-200 min-h-screen p-16 flex flex-wrap items-center justify-between">
-      
-      <HorizontalCard :doc="doc"/>
-      <HorizontalCard :doc="doc"/>
-      <HorizontalCard :doc="doc"/>
-
+    <div class="bg-gray-200 min-h-screen px-16 flex flex-wrap  py-16">
+      <HorizontalCard :doc="doc1"/>
+      <HorizontalCard :doc="doc2"/>
+      <HorizontalCard :doc="doc3"/>
+      <HorizontalCard :doc="doc4"/>
       <!-- <PropertyCard :property="property"/>
       <PropertyCard :property="property"/>
       <PropertyCard :property="property"/> -->
@@ -27,8 +26,29 @@ export default {
     LandingPage
   },
   data: () => ({
-    doc: {
-      imageURL: 'https://images.unsplash.com/photo-1518976024611-28bf4b48222e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=632&q=80',
+    doc1: {
+      imageURL: 'https://images.unsplash.com/photo-1512069772995-ec65ed45afd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=677&q=80',
+      title: 'Leveraging Compute Power for Drug Discovery',
+      course: 'ENG2003',
+      type: 'Technical Report',  
+    },
+    doc2: {
+      imageURL: 'https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+      title: 'Feedback Control Loops',
+      course: 'ENG2003',
+      type: 'Poster', 
+    },
+    doc3: {
+      imageURL: 'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/3732480/3732480-1585444835024-6e9ef9b9c9cb3.jpg',
+      title: 'Amateur Hour - Developer Podcast',
+      course: 'Personal',
+      type: 'Podcast',     
+    },
+    doc4: {
+      imageURL: 'https://images.unsplash.com/photo-1581092335331-5e00ac65e934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+      title: 'Control Loops in Software',
+      course: 'ENG2003',
+      type: 'Handout',    
     },
   })
 }
--- ReubenNinan/src/components/HorizontalCard.vue
+++ ReubenNinan/src/components/HorizontalCard.vue
@@ -3,15 +3,14 @@
 <div class="max-w-sm rounded  overflow-hidden shadow-lg">
   <img class="w-full h-auto" :src="doc.imageURL" alt="Sunset in the mountains">
   <div class="px-6 py-4">
-    <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
+    <div class="font-bold text-xl mb-2">{{doc.title}}</div>
     <p class="text-grey-darker text-base">
       Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
     </p>
   </div>
   <div class="px-6 py-4">
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#photography</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker mr-2">#travel</span>
-    <span class="inline-block bg-grey-lighter rounded-full px-3 py-1 text-sm font-semibold text-grey-darker">#winter</span>
+    <span class="inline-block bg-yellow-600 shadow-md rounded-full px-3 py-1 text-sm font-semibold mr-2">#{{doc.course}}</span>
+    <span class="inline-block bg-yellow-600 shadow-md rounded-full px-3 py-1 text-sm font-semibold mr-2">#{{doc.type}}</span>
   </div>
 </div>
 </div>
