681db924102067d533499a063a0908d8ba7650b0
Reuben Mathew <intelphysic@gmail.com>
2020-04-11 15:50:29 -0400

    added reflection section, formatting fixes

--- ReubenNinan/src/components/HorizontalCard.vue
+++ ReubenNinan/src/components/HorizontalCard.vue
@@ -1,6 +1,6 @@
 <template>
 <div class = "p-4">
-<div class="max-w-lg rounded-lg overflow-hidden shadow-xl">
+<div class="max-w-xl rounded-lg overflow-hidden shadow-lg">
   <img class="w-full h-auto" :src="doc.imageURL" alt="Sunset in the mountains">
   <div class="px-6 py-4">
     <div class="font-bold text-xl mb-2">{{doc.title}}</div>
@@ -8,9 +8,9 @@
       {{doc.content}}  
     </p>
   </div>
-  <div class="px-6 py-4 flex">
-    <span class="inline-block bg-yellow-600 shadow-md rounded-full px-3 py-2 text-sm font-semibold mr-2">#{{doc.course}}</span>
-    <span class="inline-block bg-yellow-600 shadow-md rounded-full px-3 py-2 text-sm font-semibold mr-2">#{{doc.type}}</span>
+  <div class="px-6 py-4 flex text-gray-900">
+    <span class="inline-block bg-yellow-500 shadow-lg rounded-full px-3 py-2 text-sm font-semibold mr-2">#{{doc.course}}</span>
+    <span class="inline-block bg-yellow-500 shadow-lg rounded-full px-3 py-2 text-sm font-semibold mr-2">#{{doc.type}}</span>
   </div>
 </div>
 </div>
--- ReubenNinan/src/views/Home.vue
+++ ReubenNinan/src/views/Home.vue
@@ -1,14 +1,24 @@
 <template>
-  <div id="app">
+  <div>
     <LandingPage/>
-    <div class="text-yellow-600 bg-gray-900 text-center font-semibold py-8 text-5xl">Project Showcase</div>
-    <div class="bg-gray-200 min-h-screen px-16 flex flex-wrap py-4 justify-center">
-      <HorizontalCard :doc="doc1"/>
-      <HorizontalCard :doc="doc2"/>
-      <HorizontalCard :doc="doc3"/>
-      <HorizontalCard :doc="doc4"/>
+    <!-- Divider -->
+    <Reflection/>
 
+    <!-- Project Cards -->
+    <div class="text-yellow-500 bg-gray-900 text-center font-semibold py-8 text-5xl">Project Showcase</div>
+    <div class="bg-gray-200 min-h-screen px-16 flex flex-wrap py-8 justify-center">
+      <HorizontalCard :doc="report"/>
+      <HorizontalCard :doc="doc2"/>
+      <a href="https://anchor.fm/amateur-hour/" class="hover:opacity-75">
+      <HorizontalCard :doc="amateurHour"/>
+      </a>
+      <HorizontalCard :doc="doc4"/>
     </div>
+    <!-- Footer -->
+    <div class=" text-right sm:text-center py-4 px-10 bg-yellow-500 font-semibold">
+      Made with ❤️ by Reuben
+    </div>
+    
   </div>
 </template>
 
@@ -16,37 +26,42 @@
 // import PropertyCard from './components/PropertyCard'
 import HorizontalCard from '../components/HorizontalCard.vue'
 import LandingPage from '../components/LandingPage'
+import Reflection from '../components/Reflection'
+
 export default {
   name: 'home',
   components: {
     // PropertyCard,
     HorizontalCard,
-    LandingPage
+    LandingPage,
+    Reflection,
   },
   data: () => ({
-    doc1: {
-      imageURL: 'https://images.unsplash.com/photo-1512069772995-ec65ed45afd6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=677&q=80',
+    report: {
+      imageURL: 'https://images.unsplash.com/photo-1584017911766-d451b3d0e843?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1049&q=80',
       title: 'Leveraging Compute Power for Drug Discovery',
       course: 'ENG2003',
-      type: 'Technical Report',  
+      type: 'Technical Report',
+      content: 'This is a technical report I wrote for an assignment in my ENG2003 course.'
     },
     doc2: {
-      imageURL: 'https://images.unsplash.com/photo-1581090464777-f3220bbe1b8b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
+      imageURL: 'https://images.unsplash.com/photo-1532186773960-85649e5cb70b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
       title: 'Feedback Control Loops',
       course: 'ENG2003',
       type: 'Poster', 
+      content: '',
     },
-    doc3: {
+    amateurHour: {
       imageURL: 'https://s3-us-west-2.amazonaws.com/anchor-generated-image-bank/production/podcast_uploaded_nologo400/3732480/3732480-1585444835024-6e9ef9b9c9cb3.jpg',
       title: 'Amateur Hour - Developer Podcast',
       course: 'Personal',
       type: 'Podcast',     
     },
     doc4: {
-      imageURL: 'https://images.unsplash.com/photo-1581092335331-5e00ac65e934?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60',
-      title: 'Control Loops in Software',
+      imageURL: 'https://images.unsplash.com/photo-1502780809386-f4ed7a4a4c59?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80',
+      title: 'Back of a napkin - Containerization',
       course: 'ENG2003',
-      type: 'Handout',    
+      type: 'Video',    
     },
   })
 }
